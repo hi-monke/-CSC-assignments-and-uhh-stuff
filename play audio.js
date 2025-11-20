7b8dfa2965371a9e04252a9d73049cf230d073a5
hi_monke <isaiahgardea06@gmail.com>
2025-11-20 14:46:25 -0700

    Add files via upload

---
+++ play audio.js
@@ -0,0 +1,31 @@
+// create function to add audio to html
+        function addAudio(){
+            // create new ausio element
+            let audioElement = document.createElement("audio");
+            // set id so ausio can be used in other functions
+            audioElement.setAttribute("id", "myAudio");
+            // use audio sound file
+            audioElement.setAttribute("src", "Mater-Pissed-His-Pants.mp3");
+            // this adds controll dashboard to show the audio is working
+            audioElement.setAttribute("controls", "controls");
+
+            // add new ausio element to empty div
+            document.getElementById("divAudio").appendChild(audioElement);
+
+            // hide the add audio button
+            document.getElementById("btnaddAudio").hidden = true;
+
+            // unhide the play and pause buttons
+            document.getElementById("btnPlayAudio").hidden = false;
+            document.getElementById("btnPauseAudio").hidden = false;
+        }
+
+        function playAudio(){
+            let audio = document.getElementById("myAudio");
+            audio.play();
+        }
+
+        function pauseAudio(){
+            let audio = document.getElementById("myAudio");
+            audio.pause();
+        }
